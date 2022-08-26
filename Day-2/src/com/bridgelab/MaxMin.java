556a27726f246ff7ccbb44e9fe7522335c1f2e7f
Anshul <you@example.com>
2022-08-26 21:13:45 +0530

    here we using loop and condition

---
+++ Day-2/src/com/bridgelab/MaxMin.java
@@ -0,0 +1,10 @@
+package com.bridgelab;
+
+import java.util.Scanner;
+
+public class MaxMin {
+	public static void main(String args[]) throws InterruptedException { Scanner scnr = new Scanner(System.in); // Calculating Maximum two numbers in Java System.out.println("Please enter two numbers to find maximum of two"); int a = scnr.nextInt(); int b = scnr.nextInt(); if (a > b) { System.out.printf("Between %d and %d, maximum is %d %n", a, b, a); } else { System.out.printf("Between %d and %d, maximum number is %d %n", a, b, b); } int max = Math.max(a, b); System.out.printf("Maximum value of %d and %d using Math.max() is %d %n", a, b, max); // Calculating Minimum between two numbers in Java System.out.println("Please enter two numbers to find minimum of two"); int x = scnr.nextInt(); int y = scnr.nextInt(); if (x < y) { System.out.printf("Between %d and %d, Minimum Number is %d %n", x, y, x); } else { System.out.printf("Between %d and %d, Minimum is %d %n", x, y, y); } int min = Math.min(x, y); System.out.printf("Maximum value of %d and %d using Math.min() is %d %n", x, y, min); }
+
+
+	}
+}
